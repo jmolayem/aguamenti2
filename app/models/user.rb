b15eb5533bf33d48c88823c3268ea4d97bbf4579
jmolayem <jmmolaye@usc.edu>
2015-08-07 15:59:23 -0700

    add admin field

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-email_regex = /\A[\w+\-.]+@usc\.edu\z/i
    validates :name, presence: true
    validates :email, :presence => true,
-                  :format   => { :with => email_regex },
                   :uniqueness => { :case_sensitive => false }
    has_many :models, dependent: :destroy
    has_many :results, dependent: :destroy
