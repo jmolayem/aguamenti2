eac94490f2f3d6834ff4d293a6d5d9ad3e50468a
jmolayem <jmmolaye@usc.edu>
2015-08-22 09:24:09 -0700

    added gifs to home, renewed search web service for index, first attempt at defining language web service

---
+++ app/services/language_learning.rb
@@ -0,0 +1,27 @@
+class LanguageLearning
+  ENDPOINT = "https://www.metamind.io/language/classify"
+
+  def self.post_language(api_key, file)
+    new(api_key).post_language(iterations, file)
+  end
+
+  def initialize(api_key)
+    @endpoint = ENDPOINT
+    @api_key = api_key
+  end
+
+  private
+
+  def headers_for_post
+    {
+      accept: :json
+    }
+  end
+
+  def build_payload(iterations, file)
+    {
+      'key' => @api_key,
+      'file-0' => file
+    }
+  end
+end
---
+++ app/workers/language_learning_worker.rb
@@ -0,0 +1,4 @@
+class LanguageLearningWorker
+  include Sidekiq::Worker
+
+end
