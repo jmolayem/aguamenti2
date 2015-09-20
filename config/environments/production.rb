2f6f592deb3299d0b54f5724dd8ef0b42be2b6fd
jmolayem <jmmolaye@usc.edu>
2015-09-20 14:03:05 -0700

    Configuring S3 on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,13 @@ Rails.application.configure do
     :domain         => 'www.govmind.org',
     :enable_starttls_auto => true
   }
+  
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
