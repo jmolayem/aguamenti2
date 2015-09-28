aa70e88d2a9a6561d2a1f3b986581a273b5f6dae
jmolayem <jmmolaye@usc.edu>
2015-09-27 17:01:31 -0700

    attempt at fixing facebook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 #OmniAuth.config.logger = Rails.logger
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'], scope: 'public_profile'
-end
+#Rails.application.config.middleware.use OmniAuth::Builder do
+#  provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'], scope: 'public_profile'
+#end
 #Rails.application.config.middleware.user OmniAuth::builder do
 #config.omniauth :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET']
 #end
