ffb7d799cbbd0d9f03bda719d334103c3f4f4e0a
jmolayem <jmmolaye@usc.edu>
2015-10-06 19:33:02 -0700

    showed more file information on the data galleries page

--- app/helpers/natlangs_helper.rb
+++ app/helpers/natlangs_helper.rb
@@ -1,2 +1,7 @@
 module NatlangsHelper
+	def predictions_for(result)
+		result.predictions.map do |prediction|
+			"%s: %2.1f%" % [prediction.class_name, prediction.probability * 100]
+		end.join(', ')
+	end
 end
